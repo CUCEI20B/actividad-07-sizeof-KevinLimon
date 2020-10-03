c292f57cf19823b105e0dadaa6195aba50b2b2e2
Kevin Limón <kevin.limon7482@alumnos.udg.mx>
2020-10-03 13:36:25 -0500

    Agregamos el código haciendo uso de estructuras if, el código funciona bien

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string dato;
+    getline(cin, dato);
+    if(dato=="char"){
+        cout<<sizeof(int)<<endl;
+    }
+    else if(dato=="int"){
+        cout<<sizeof(char)<<endl;
+    }
+    else if(dato=="short"){
+        cout<<sizeof(short)<<endl;
+    }
+    else if(dato=="float"){
+        cout<<sizeof(float)<<endl;
+    }
+    else if(dato=="double"){
+        cout<<sizeof(double)<<endl;
+    }
+    else if(dato=="long"){
+        cout<<sizeof(long)<<endl;
+    }
+    else{
+        cout<<"El dato no es valido"<<endl;
+    }
 
     return 0;
 }
